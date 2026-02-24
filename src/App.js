511664c1d709d744bbc6ccb5c8bcfc89e9bda8d4
wsuits6 <wsuits6@gmail.com>
2026-02-24 18:13:10 +0000

    added State managment to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  // Declare state
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+
+      <h2>{count}</h2>
+
+      <button
+        onClick={() => {
+          setCount(prev => prev + 1);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
